6ea42cbd9c3005a43fbb986996a39846c934852d
Brandon <brandon.gavett@uwa.edu.au>
2019-10-19 12:58:22 +0800

    Initial commit

---
+++ R/addins.R
@@ -0,0 +1,92 @@
+cs_Machine_Learning_Modelling_in_R <- function(){cs('Machine Learning Modelling in R')}
+
+ cs_base_r <- function(){cs('base-r')}
+
+ cs_caret <- function(){cs('caret')}
+
+ cs_cartography <- function(){cs('cartography')}
+
+ cs_data_import <- function(){cs('data-import')}
+
+ cs_data_transformation <- function(){cs('data-transformation')}
+
+ cs_data_visualization_2.1 <- function(){cs('data-visualization-2.1')}
+
+ cs_datatable <- function(){cs('datatable')}
+
+ cs_declaredesign <- function(){cs('declaredesign')}
+
+ cs_distr6 <- function(){cs('distr6')}
+
+ cs_estimatr <- function(){cs('estimatr')}
+
+ cs_eurostat <- function(){cs('eurostat')}
+
+ cs_factors <- function(){cs('factors')}
+
+ cs_gganimate <- function(){cs('gganimate')}
+
+ cs_golem <- function(){cs('golem')}
+
+ cs_gwasrapidd <- function(){cs('gwasrapidd')}
+
+ cs_h2o <- function(){cs('h2o')}
+
+ cs_how_big_is_your_graph <- function(){cs('how-big-is-your-graph')}
+
+ cs_keras <- function(){cs('keras')}
+
+ cs_leaflet <- function(){cs('leaflet')}
+
+ cs_lubridate <- function(){cs('lubridate')}
+
+ cs_mlr <- function(){cs('mlr')}
+
+ cs_mosaic <- function(){cs('mosaic')}
+
+ cs_nardl <- function(){cs('nardl')}
+
+ cs_package_development <- function(){cs('package-development')}
+
+ cs_parallel_computation <- function(){cs('parallel_computation')}
+
+ cs_purrr <- function(){cs('purrr')}
+
+ cs_quanteda <- function(){cs('quanteda')}
+
+ cs_randomizr <- function(){cs('randomizr')}
+
+ cs_regex <- function(){cs('regex')}
+
+ cs_reticulate <- function(){cs('reticulate')}
+
+ cs_rmarkdown_2.0 <- function(){cs('rmarkdown-2.0')}
+
+ cs_rphylopic <- function(){cs('rphylopic')}
+
+ cs_rstudio_ide <- function(){cs('rstudio-ide')}
+
+ cs_sf <- function(){cs('sf')}
+
+ cs_shiny <- function(){cs('shiny')}
+
+ cs_sjmisc <- function(){cs('sjmisc')}
+
+ cs_sparklyr <- function(){cs('sparklyr')}
+
+ cs_strings <- function(){cs('strings')}
+
+ cs_survminer <- function(){cs('survminer')}
+
+ cs_syntax <- function(){cs('syntax')}
+
+ cs_teachR <- function(){cs('teachR')}
+
+ cs_tidyeval <- function(){cs('tidyeval')}
+
+ cs_tsbox <- function(){cs('tsbox')}
+
+ cs_vtree <- function(){cs('vtree')}
+
+ cs_xplain <- function(){cs('xplain')}
+
---
+++ R/cheatsheet.R
@@ -0,0 +1,33 @@
+csOptions <- function(){
+  csURL <- "https://github.com/rstudio/cheatsheets"
+  csOpts <- csURL %>%
+    xml2::read_html() %>%
+    rvest::html_text() %>%
+    stringr::str_extract_all(".+[.]pdf") %>%
+    unlist() %>%
+    trimws() %>%
+    gsub(., pattern = ".pdf$", replacement = "")
+  csOpts[!(csOpts %in% c("0-template", "Update quanteda", "New version of regex"))]
+}
+
+cs <- function(sheetname = "base-r"){
+  sheetname <- gsub(pattern = " ", replacement = "%20", x = sheetname)
+  mdFile <- tempfile(pattern = "rmd", fileext = ".Rmd")
+  writeLines(paste0('
+---
+output: html_document
+---
+
+```{r image, echo = FALSE}
+sheetURL <- "https://github.com/rstudio/cheatsheets/raw/master/', sheetname, '.pdf"
+tempSheet <- tempfile(pattern = "temp", fileext = ".pdf")
+download.file(sheetURL, tempSheet, mode = "wb", quiet = TRUE)
+x <- suppressMessages(pdftools::pdf_convert(tempSheet, format = "png", verbose = FALSE, dpi = 300))
+knitr::include_graphics(x)
+```
+'), con = mdFile)
+
+  tempOut <- tempfile(pattern = "cheat", fileext = ".html")
+  rmarkdown::render(mdFile, output_file = tempOut, quiet = TRUE)
+  try(rstudioapi::viewer(tempOut), silent = TRUE)
+}
